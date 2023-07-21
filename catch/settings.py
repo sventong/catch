c396919df1ddea693c1c0f581499d5508dd9878b
stong <sven.ktj@gmail.com>
2023-07-21 16:06:30 +0200

    csrf

--- catch/settings.py
+++ catch/settings.py
@@ -176,3 +176,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["catch-me-c1ad50dcb72d.herokuapp.com/"]
