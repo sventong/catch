d3f5ad9ce01c301e3733a7fa2f024903d4b44c4f
stong <sven.ktj@gmail.com>
2023-07-21 12:53:49 +0200

    settings

--- catch/catch/settings.py
+++ catch/catch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u96l3tzte06=u4%ek@wz$+4ea0z2*z%xi7fldbjlf&yuv-qu!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
